d1d9c47a15c35c1a0d0ef8bfbed26eea61ebcb17
Kentahoriuchi <dorahori108@yahoo.co.jp>
2023-01-23 01:06:27 +0900

    main&initファイル作製

---
+++ baseball/__init__.py
@@ -0,0 +1,11 @@
+import game
+
+
+class Init:
+
+    def __init__(self):
+        pass
+
+    def play_game(self):
+        play_ground = game.Game()
+        play_ground.start_game()
---
+++ baseball/__main__.py
@@ -0,0 +1,12 @@
+import game
+
+
+def main():
+    # mainの処理
+    play_ground = game.Game()
+    play_ground.start_game()
+
+
+if __name__ == '__main__':
+    main()
+
