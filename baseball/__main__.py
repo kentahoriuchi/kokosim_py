a5f39dca2e038a43e3a75f7c34975edca61211e9
Kentahoriuchi <dorahori108@yahoo.co.jp>
2023-01-31 03:05:03 +0900

    randomな選手を作製する（名前のデータも追加）

--- baseball/__main__.py
+++ baseball/__main__.py
@@ -1,14 +1,17 @@
 from game import Game
-from team_generator import normal_team_generate
+from team_generator import ramdom_team_generate
+from baseball.scripts.write_csv import write_csv_team
 
 
 def main():
     # mainの処理
-    first_team, second_team = normal_team_generate()
-    play_ground = Game(first_team, second_team)
-    play_ground.start_game()
-    first_score, second_score = play_ground.get_score()
-    print("%d vs %d" %(first_score, second_score))
+    first_team, second_team = ramdom_team_generate()
+    for i in range(100):
+        play_ground = Game(first_team, second_team)
+        play_ground.start_game()
+        first_score, second_score = play_ground.get_score()
+        print("%d vs %d" %(first_score, second_score))
+    write_csv_team(first_team)
 
 
 if __name__ == '__main__':
--- baseball/team_generator.py
+++ baseball/team_generator.py
@@ -1,18 +1,83 @@
 from entity.pitcher import Pitcher
 from entity.batter import Batter
 from entity.team import Team
+from random import randint
+from scripts.create_name import create
 
 
-def normal_team_generate():
+def normal_team_generate() -> (Team, Team):
     first_team = Team()
     second_team = Team()
-    player_list = []
+    player_list1 = []
+    player_list2 = []
+    for _ in range(9):
+        batter = Batter.ButterBuilder()\
+            .name("バッター")\
+            .contact(50)\
+            .power(50)\
+            .run(50)\
+            .defence(50)\
+            .throw(50)\
+            .build()
+        player_list1.append(batter)
+    pitcher1 = Pitcher.PitcherBuilder()\
+        .name("ピッチャー")\
+        .speed(50)\
+        .control(50)\
+        .henka(50)\
+        .stamina(50)\
+        .build()
+    first_team.set_players(player_list1)
+    first_team.set_pitcher(pitcher1)
     for _ in range(9):
         batter = Batter.ButterBuilder().name("バッター").contact(50).power(50).run(50).defence(50).throw(50).build()
-        player_list.append(batter)
-    pitcher = Pitcher.PitcherBuilder().name("ピッチャー").speed(50).control(50).henka(50).stamina(50).build()
-    first_team.set_players(player_list)
-    first_team.set_pitcher(pitcher)
-    second_team.set_players(player_list)
-    second_team.set_pitcher(pitcher)
+        player_list2.append(batter)
+    pitcher2 = Pitcher.PitcherBuilder().name("ピッチャー").speed(50).control(50).henka(50).stamina(50).build()
+    second_team.set_players(player_list2)
+    second_team.set_pitcher(pitcher2)
+    return first_team, second_team
+
+def ramdom_team_generate() -> (Team, Team):
+    first_team = Team()
+    second_team = Team()
+    player_list1 = []
+    player_list2 = []
+    for _ in range(9):
+        batter = Batter.ButterBuilder()\
+            .name(create())\
+            .contact(randint(1, 100))\
+            .power(randint(1, 100))\
+            .run(randint(1, 100))\
+            .defence(randint(1, 100))\
+            .throw(randint(1, 100))\
+            .build()
+        player_list1.append(batter)
+    pitcher1 = Pitcher.PitcherBuilder()\
+        .name(create())\
+        .speed(randint(1, 100))\
+        .control(randint(1, 100))\
+        .henka(randint(1, 100))\
+        .stamina(randint(1, 100))\
+        .build()
+    first_team.set_players(player_list1)
+    first_team.set_pitcher(pitcher1)
+    for _ in range(9):
+        batter = Batter.ButterBuilder() \
+            .name(create()) \
+            .contact(randint(1, 100)) \
+            .power(randint(1, 100)) \
+            .run(randint(1, 100)) \
+            .defence(randint(1, 100)) \
+            .throw(randint(1, 100)) \
+            .build()
+        player_list2.append(batter)
+    pitcher2 = Pitcher.PitcherBuilder() \
+        .name(create()) \
+        .speed(randint(1, 100)) \
+        .control(randint(1, 100)) \
+        .henka(randint(1, 100)) \
+        .stamina(randint(1, 100)) \
+        .build()
+    second_team.set_players(player_list2)
+    second_team.set_pitcher(pitcher2)
     return first_team, second_team
