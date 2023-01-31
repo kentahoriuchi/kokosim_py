8b0e03ad49beecaddcf511153e672943de1fbf7e
Kentahoriuchi <dorahori108@yahoo.co.jp>
2023-02-01 01:52:31 +0900

    チームジェネレータ更新（optimize追加）

--- baseball/team_generator.py
+++ baseball/team_generator.py
@@ -1,16 +1,16 @@
 from entity.pitcher import Pitcher
 from entity.batter import Batter
 from entity.team import Team
+from game import Game
 from random import randint
+from random import shuffle
 from scripts.create_name import create_player_name
 from scripts.create_name import create_team_name
 
 
-def normal_team_generate() -> (Team, Team):
-    first_team = Team()
-    second_team = Team()
-    player_list1 = []
-    player_list2 = []
+def normal_team_generate() -> Team:
+    team = Team()
+    player_list = []
     for _ in range(9):
         batter = Batter.ButterBuilder()\
             .name("バッター")\
@@ -20,31 +20,23 @@ def normal_team_generate() -> (Team, Team):
             .defence(50)\
             .throw(50)\
             .build()
-        player_list1.append(batter)
-    pitcher1 = Pitcher.PitcherBuilder()\
+        player_list.append(batter)
+    pitcher = Pitcher.PitcherBuilder()\
         .name("ピッチャー")\
         .speed(50)\
         .control(50)\
         .henka(50)\
         .stamina(50)\
         .build()
-    first_team.set_players(player_list1)
-    first_team.set_pitcher(pitcher1)
-    for _ in range(9):
-        batter = Batter.ButterBuilder().name("バッター").contact(50).power(50).run(50).defence(50).throw(50).build()
-        player_list2.append(batter)
-    pitcher2 = Pitcher.PitcherBuilder().name("ピッチャー").speed(50).control(50).henka(50).stamina(50).build()
-    second_team.set_players(player_list2)
-    second_team.set_pitcher(pitcher2)
-    return first_team, second_team
+    team.set_players(player_list)
+    team.set_pitcher(pitcher)
+    return team
 
-def ramdom_team_generate() -> (Team, Team):
-    first_team = Team()
-    second_team = Team()
-    first_team.set_name(create_team_name())
-    second_team.set_name(create_team_name())
-    player_list1 = []
-    player_list2 = []
+
+def random_team_generate() -> Team:
+    team = Team()
+    team.set_name(create_team_name())
+    player_list = []
     for _ in range(9):
         batter = Batter.ButterBuilder()\
             .name(create_player_name())\
@@ -54,33 +46,48 @@ def ramdom_team_generate() -> (Team, Team):
             .defence(randint(1, 100))\
             .throw(randint(1, 100))\
             .build()
-        player_list1.append(batter)
-    pitcher1 = Pitcher.PitcherBuilder()\
+        player_list.append(batter)
+    pitcher = Pitcher.PitcherBuilder()\
         .name(create_player_name())\
         .speed(randint(1, 100))\
         .control(randint(1, 100))\
         .henka(randint(1, 100))\
         .stamina(randint(1, 100))\
         .build()
-    first_team.set_players(player_list1)
-    first_team.set_pitcher(pitcher1)
-    for _ in range(9):
-        batter = Batter.ButterBuilder() \
-            .name(create_player_name()) \
-            .contact(randint(1, 100)) \
-            .power(randint(1, 100)) \
-            .run(randint(1, 100)) \
-            .defence(randint(1, 100)) \
-            .throw(randint(1, 100)) \
+    # player_list = optimize_player_list(player_list)
+    team.set_players(player_list)
+    team.set_pitcher(pitcher)
+    return team
+
+
+def optimize_player_list(player_list):
+    best_order = player_list
+    best_score = 0
+    normal_team = normal_team_generate()
+    for _ in range(100):
+        print(best_order)
+        print(best_score)
+        candidate_list = best_order
+        shuffle(candidate_list)
+        candidate_team = Team()
+        candidate_team.set_players(candidate_list)
+        pitcher = Pitcher.PitcherBuilder() \
+            .name("ピッチャー") \
+            .speed(50) \
+            .control(50) \
+            .henka(50) \
+            .stamina(50) \
             .build()
-        player_list2.append(batter)
-    pitcher2 = Pitcher.PitcherBuilder() \
-        .name(create_player_name()) \
-        .speed(randint(1, 100)) \
-        .control(randint(1, 100)) \
-        .henka(randint(1, 100)) \
-        .stamina(randint(1, 100)) \
-        .build()
-    second_team.set_players(player_list2)
-    second_team.set_pitcher(pitcher2)
-    return first_team, second_team
+        candidate_team.set_pitcher(pitcher)
+        score = 0
+        for _ in range(30):
+            game = Game(candidate_team, normal_team, False)
+            game.start_game()
+            tmp_score, _ = game.get_score()
+            score += tmp_score
+        if score > best_score:
+            best_order = candidate_list
+            best_score = score
+
+    return best_order
+
