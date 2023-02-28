678be88b9190838245413921714b09c1db3eb28c
Kentahoriuchi <dorahori108@yahoo.co.jp>
2023-03-01 02:06:21 +0900

    main雛形作製

--- baseball/__main__.py
+++ baseball/__main__.py
@@ -1,18 +1,10 @@
-from game import Game
-from team_generator import ramdom_team_generate
-from baseball.scripts.write_csv import write_csv_team
-
-
 def main():
-    # mainの処理
-    first_team, second_team = ramdom_team_generate()
-    for i in range(100):
-        play_ground = Game(first_team, second_team)
-        play_ground.start_game()
-        first_score, second_score = play_ground.get_score()
-        print("%d vs %d" %(first_score, second_score))
-    write_csv_team(first_team)
-    write_csv_team(second_team)
+    # ゲームの起動時処理
+    start()
+    # ゲーム中処理
+    do()
+    # ゲーム終了時処理
+    finish()
 
 
 if __name__ == '__main__':
