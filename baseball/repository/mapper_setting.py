3dab84dc6d93591a68a6c149d8f69f890b943a23
Kentahoriuchi <dorahori108@yahoo.co.jp>
2023-03-03 02:53:32 +0900

    session追加

--- baseball/repository/mapper_setting.py
+++ baseball/repository/mapper_setting.py
@@ -1,6 +1,7 @@
 import sqlalchemy
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 db_name = "test_db"
 Base = declarative_base()
 Engine = sqlalchemy.create_engine('sqlite:///' + db_name + '.db', echo=True)
+Session = sessionmaker(bind=Engine)()
